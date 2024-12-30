0b18fed755d40fa3f7b37390fe325e4cc615c03b
Gonzalo Díaz <69087423+gfdiazc@users.noreply.github.com>
2024-12-30 14:58:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ def setup_webdriver():
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.binary_location = "/usr/bin/chromium-browser"  # Specify the Chromium binary location
         
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
